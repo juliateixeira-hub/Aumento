c9469164b79b4c75959b34236ecc53f58dbcbdc9
Júlia Teixeira <julia_vlj@hotmail.com>
2022-08-31 14:35:57 -0300

    algumas alterações foram feitas mas preciso revisar

--- Lista17.js
+++ Lista17.js
@@ -1,38 +1,17 @@
-/*Essa função tem que receber dois parâmetros: o salário e a %
-de aumento. E retornar o valor do salário já com o aumento.
-3. Depois de calcular o aumento você deve exibir: o nome do
-colaborador, o salário, a % de aumento e o salário reajustado.
-4. Crie uma função que pergunte se o usuário deseja calcular
-novamente com novas informações.
-Para calcular o aumento, faça o valor do salário multiplicado pela
-porcentagem em decimal.
-Ex.: salário R$1.000,00 aumento de 10%
-1000 * 1.10 = 1100
-salário R$1.000,00 aumento de 15%
-1000 * 1.15 = 1150
-ATENÇÃO!! Faça um commit para cada item do exercício.*/
 var continuar = 1
-
+var NovoSalario = 0
 
 PerguntarNome()
 Salário()
-AumentoSalário()
+CalculoAumento(Salário, Porcentagem)
+Porcentagem(Salário)
+CalcularNovamente()
 
 while (continuar == 1) {
-if (Salário <= 1500){
-    console.log("Aumento de 20%")
-}
-else if (Salário <= 2000){
-    console.log("Aumento de 15%")
-    Salário = 1501
-}
-else if (Salário <= 3000){
-    console.log("Aumento de 10%")
-    Salário = 2001
-}
-else if(Salário >= 3001){
-    console.log("Aumento de 5%")
-}
+    Porcentagem(Salário)
+    NovoSalario = CalculoAumento(Salário, AumentoSalário)
+    console.log(PerguntarNome() + " - " + Salário + " - " + Porcentagem + " NovoSalario "  ) 
+    console.log(prompt("Deseja calcular novamente? 1 - sim / 2 - não "))
 }
 
 function PerguntarNome() {
@@ -41,7 +20,24 @@ function PerguntarNome() {
 function Salário() {
     console.log(parseFloat(prompt("Qual é o seu salário?")))
 }
-function AumentoSalário() {
-    for (var contador = 0; contador < Salário; contador++)
-        contador++
+function CalculoAumento(Salário, AumentoSalário) {
+    return Salário + (Salário * (AumentoSalário / 100))
+}
+
+function Porcentagem(Salário) {
+    if (Salário <= 1500){
+        return 20
+    }
+    else if (Salário <= 2000) {
+        return 15
+    }
+    else if (Salário <= 3000) {
+        return 10
+    }
+    else if (Salário <= 3001){
+        return 5
+    }
+}
+function CalcularNovamente(){
+    return CalcularNovamente()
 }
